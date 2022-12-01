9ff3aabf76ca8800a1468fe61d73771de5296056
SudhaRatan <rattankumar2511@gmail.com>
2022-12-01 23:41:55 +0530

    qwe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./components/homeStyle.css"
+import { useNavigate } from "react-router-dom";
 
 export const API = process.env.REACT_APP_API;
 
 function App() {
-  
+
+  const navigate = useNavigate();
+
   return (
-    
+
     <div className="Home">
-    <div className="feat">Featuring section</div>
+      <div className="feat" onClick={()=>navigate("/product")}>Featuring section</div>
       <div className="elec"></div>
       <div className="clothes"></div>
       <div className="toys"></div>
